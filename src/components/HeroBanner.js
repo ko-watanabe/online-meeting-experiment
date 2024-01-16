f9a2413fdc261593b27d9d85bde561ae3cd77c6f
ko-watanabe <ko.watanabe.0522@gmail.com>
2024-01-16 10:13:07 +0100

    Add experiment detail

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -10,16 +10,29 @@ function HeroBanner() {
           <div className="home-text">
             {/* <div className="section-text__subtitle">Explore the whiskers</div> */}
             <div className="section-text__title-big">
-              About experiment
+              About Experiment
             </div>
             <div className="section-text__body">
-              Introducing our innovative video call app, seamlessly connecting users through high-quality video and audio communication. Enjoy group calls, screen sharing, and secure conversations, redefining virtual interactions for both personal and professional use.
+              <h1> First of all </h1>
+              Thank you for participating our experiment.
+              This experiment aim to measure the engagement levels while conducting online video calls.
+              Your task is to join the meeting and discuss about the topic presented by the experiment organizer.
+              The experiment is not forced to participate and can be opt-out anytime when you feel uncomfortable.
+              <h1> Experiment Details </h1>
+              Our experiment will collect your facial recordings.
+              The screenshot may be used for the scientific publications or in slide.
+              You will be joining three meetings maximum.
+              Each time the topic will be different.
+              Once you finish the meeting we will ask a post-survey to fill-out about the meeting.
+              The survey will be in google form.
+              <h1> Let's begin </h1>
+              To begin our experiment, we would like to ask you to login to this application.
+              Please click the button below.
+              Once you finish login or signup, tell experiment conductor the situation.
             </div>
             <a href="#download">
-              <Link to='/video-app' style={{ color: "whitesmoke" }}>
-
-                <button className="call-btn">Create A Metting
-                </button>
+              <Link to='/login' style={{ color: "whitesmoke" }}>
+                <button className="call-btn">Login / Signup</button>
               </Link>
             </a>
           </div>
